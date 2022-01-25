4e19511a4aabe381d42ea9d8b9090533874fd325
“tiagorahal” <rahal.aires@gmail.com>
2022-01-24 22:19:59 -0300

    Call quickFacts method and pickSubstituteTeacher static method passing an array as argument

--- main.js
+++ main.js
@@ -58,3 +58,5 @@ class HighSchool extends School {
 }
 
 const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+lorraineHansbury.quickFacts();
+const sub = School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
