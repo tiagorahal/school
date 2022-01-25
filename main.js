eef8b428ee4de5d2a1c08a1bbfb42812abbd8b4c
“tiagorahal” <rahal.aires@gmail.com>
2022-01-24 22:17:36 -0300

    Create HighSchool subclass

--- main.js
+++ main.js
@@ -45,3 +45,14 @@ class PrimarySchool extends School {
     return this._pickupPolicy;
   }
 }
+
+class HighSchool extends School {
+  constructor(name, numberOfStudents, sportsTeams) {
+    super(name, 'high', numberOfStudents);
+    this._sportsTeams = sportsTeams;
+  }
+
+  get sportsTeams() {
+    return this._sportsTeams;
+  }
+}
